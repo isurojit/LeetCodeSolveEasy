abb140616407bf9e64b65c2b7b14fff9353f2ebb
isurojit <iamsurojit@hotmail.com>
2024-04-01 22:22:05 +0530

    add binary solved

--- addBinary/javaScript/addBinary.js
+++ addBinary/javaScript/addBinary.js
@@ -3,4 +3,24 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function (a, b) {};
+var addBinary = function (a, b) {
+  const ans = [];
+  let carry = 0;
+  let i = a.length - 1;
+  let j = b.length - 1;
+
+  while (i >= 0 || j >= 0 || carry) {
+    if (i >= 0) {
+      carry += parseInt(a[i]);
+      i -= 1;
+    }
+    if (j >= 0) {
+      carry += parseInt(b[j]);
+      j -= 1;
+    }
+    ans.push(String(carry % 2));
+    carry = Math.floor(carry / 2);
+  }
+
+  return ans.reverse().join("");
+};
--- addBinary/javaScript/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === "1") {
-        result += Math.pow(2, i);
-      }
-    }
-    console.log(arr);
