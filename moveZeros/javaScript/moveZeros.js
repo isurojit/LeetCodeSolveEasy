fcfa8ad70ec1b3329e4b175681fc3f008193e0a5
isurojit <iamsurojit@hotmail.com>
2024-04-08 21:29:39 +0530

    move zeros solved

---
+++ moveZeros/javaScript/moveZeros.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== 0) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  for (let k = i; k < nums.length; k++) {
+    nums[k] = 0;
+  }
+  return nums;
+};
