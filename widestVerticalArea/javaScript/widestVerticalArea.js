5a196fa2e02c960c847ae293e537e4b4d0a36718
isurojit <iamsurojit@hotmail.com>
2024-04-12 22:48:05 +0530

    widest vertical area solved

---
+++ widestVerticalArea/javaScript/widestVerticalArea.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxWidthOfVerticalArea = function (points) {
+  points.sort((a, b) => a[0] - b[0]);
+  let maxWidth = 0;
+  for (let i = 1; i < points.length; i++) {
+    maxWidth = Math.max(maxWidth, points[i][0] - points[i - 1][0]);
+  }
+  return maxWidth;
+};
