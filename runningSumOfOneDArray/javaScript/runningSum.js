5d4851496b6d315e0ac79e303bc40ba73fdcd773
isurojit <iamsurojit@hotmail.com>
2024-04-14 21:21:55 +0530

    solved smaller than the current

---
+++ runningSumOfOneDArray/javaScript/runningSum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  let result = [];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    result.push(sum);
+  }
+  return result;
+};
