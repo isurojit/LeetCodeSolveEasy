5c87d7b23237f8dead5d85213ca5b94f592a56df
isurojit <iamsurojit@hotmail.com>
2024-04-26 23:21:02 +0530

    maximum product accepted in a pair

---
+++ maximumProductDiff/javaScript/maximumProduct.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function (nums) {
+  nums.sort((a, b) => b - a);
+  return nums[0] * nums[1] - nums[nums.length - 1] * nums[nums.length - 2];
+};
