52cc2f609f743bcbc84a3d0eff57185b317442f5
isurojit <iamsurojit@hotmail.com>
2024-04-27 23:44:10 +0530

    maximum product of two element array

--- maximumProductOfTwoElement/javaScript/maximumProduct.js
+++ maximumProductOfTwoElement/javaScript/maximumProduct.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var maxProduct = function (nums) {
-  return nums.sort((a, b) => a - b);
+  let max1 = -Infinity;
+  let max2 = -Infinity;
+
+  for (let num of nums) {
+    if (num >= max1) {
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max2 = num;
+    }
+  }
+  return (max1 - 1) * (max2 - 1);
 };
