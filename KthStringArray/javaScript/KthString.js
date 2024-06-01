0f228933fdc2e5a531f82ba99d0b7aada072df63
isurojit <iamsurojit@hotmail.com>
2024-06-01 23:27:36 +0530

    Solved Kth String

---
+++ KthStringArray/javaScript/KthString.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} arr
+ * @param {number} k
+ * @return {string}
+ */
+var kthDistinct = function (arr, k) {
+  const count = {};
+
+  // Count the occurrences of each string in the array
+  for (const a of arr) {
+    if (count[a] === undefined) {
+      count[a] = 1;
+    } else {
+      count[a]++;
+    }
+  }
+
+  // Iterate through the array and find the k-th distinct string
+  for (const a of arr) {
+    if (count[a] === 1) {
+      k--;
+      if (k === 0) {
+        return a;
+      }
+    }
+  }
+
+  return "";
+};
