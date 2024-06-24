c81560b0acd8ee14cb46c1908c0e098ae672ee60
isurojit <iamsurojit@hotmail.com>
2024-06-24 08:55:20 +0530

    solved time needed for bus ticket

---
+++ timeForBusTicket/javaScript/busTicket.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} tickets
+ * @param {number} k
+ * @return {number}
+ */
+var timeRequiredToBuy = function (tickets, k) {
+  let ans = 0;
+
+  for (let i = 0; i < tickets.length; i++) {
+    if (i <= k) {
+      ans += Math.min(tickets[i], tickets[k]);
+    } else {
+      ans += Math.min(tickets[i], tickets[k] - 1);
+    }
+  }
+
+  return ans;
+};
