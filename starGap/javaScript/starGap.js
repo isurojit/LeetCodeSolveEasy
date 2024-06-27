f4dfd91e2142c6fef605de56c8f80fee8587320b
isurojit <iamsurojit@hotmail.com>
2024-06-27 23:25:29 +0530

    solved star gap

---
+++ starGap/javaScript/starGap.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} edges
+ * @return {number}
+ */
+var findCenter = function (edges) {
+  return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]
+    ? edges[0][0]
+    : edges[0][1];
+};
